7702cc305d41a47e9136238719e9a27d13f8151e
adedejiadebusuyi <aduraraphael8@gmail.com>
2022-09-08 17:03:43 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * return - Always 0 (Success)
+ * Return: Always 0 (Success)
+ *
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0)
 }
